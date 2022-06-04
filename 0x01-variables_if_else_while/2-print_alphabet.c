c77e44eb103f98ce5bb3c6480faad8610ec44888
haymanottse11 <haymanottse11@gmail.com>
2022-06-03 19:41:34 -0700

    if else and while projects

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - printout the alphabets in lowercase
-(*
- *Return: 0 on success
+ * main - printout the alphabets in lowercase not allowed
+ * to use printf or puts, and use putchar 2X maximum
+(* 
+ * Return: 0 on success
  */
 int main(void)
 {
 	char c = 'a'; 
 
-	while(c <= 'z')  
+	while (c <= 'z')  
 	{
 		putchar(c);
 		c++;
