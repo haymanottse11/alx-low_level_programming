c38c527be5a6099bf585328578888eaa1fa8251c
haymanottse11 <haymanottse11@gmail.com>
2022-06-03 20:00:02 -0700

    if else and while projects

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - prints all possible combinations of single digit numbers
- * not allowed to use printf or puts
+ * not allowed to use printf or puts, and
  * allowed to use putchar 4X maximum 
 (*
  * Return: 0 on success
@@ -10,13 +10,13 @@ int main(void)
 {
 	int i = '0';
 
-        while (i <= '9')
+        while(i <= '9')
 	{
 		putchar(i);
 		if (i != '9')
 		{
 			putchar(',');
-			putchar('');
+			putchar(' ');
 		}
 		++i;
 	}
