26cece83a619f4d0d2311f9519f2cec27c5eb73e
haymanottse11 <haymanottse11@gmail.com>
2022-06-06 14:21:31 -0700

    hello world projects

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - returns what is entered
+ * not allowed to use printfallowed only to use puts
  *
  * Return 0 on success   
  */
 int main(void)
 {
-	char ques4[] = "\"Programming is like building a multilingual puzzle";
+char ques4[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(ques4);
-	return (0);
+puts(ques4);
+return (0);
 }
