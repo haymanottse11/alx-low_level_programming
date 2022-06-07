f5e3154126f142ac664fd2f679fe8ede042430f3
haymanottse11 <haymanottse11@gmail.com>
2022-06-07 11:47:08 -0700

    nested loop projects

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,18 @@
 void print_alphabet(void)
 {
 	char c = 'a';
-
+	
 	while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
 	}
 	_putchar("/n")
+}
+
+int main(void)
+{
+	print_alphabet();
+	
+	return (0);
 }
