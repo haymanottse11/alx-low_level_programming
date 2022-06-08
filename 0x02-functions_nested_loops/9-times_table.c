69a35f46b5aa06df3cf780fbdaf9f233e2b2a2ef
haymanottse11 <haymanottse11@gmail.com>
2022-06-08 16:01:18 -0700

    nested loops projects

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 	{
 		for (mult = 0; mult <= 9; mult++)
 		{
-			_putchar(' ');
+			_putchar(' ', ',');
 
 			prod = mult * n;
 
@@ -22,7 +22,6 @@ void times_table(void)
 				_putchar((prod / 10) + '0');
 
 			 _putchar((prod % 10) + '0');
-			 _putchar(',');
 		}
 		_putchar('\n');
 	}
