1808667ec1638dbcc2ad4f95e484f908a423ff7f
haymanottse11 <haymanottse11@gmail.com>
2022-06-08 15:36:25 -0700

    nested loops projects

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,11 +18,11 @@ void times_table(void)
 
 			if (prod <= 9)
 				_putchar(' ');
+				_putchar(',');
 			else
 				_putchar((prod / 10) + '0');
 
 			 _putchar((prod % 10) + '0');
-			 _putchar(',');
 		}
 		_putchar('\n');
 	}
