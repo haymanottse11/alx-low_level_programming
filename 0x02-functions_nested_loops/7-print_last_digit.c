24b1d85f589c752d5af8ff22015e7b630f6a80ee
haymanottse11 <haymanottse11@gmail.com>
2022-06-08 08:01:45 -0700

    nested loops projects

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	n = abs(n);
-	int last_digit = n % 10;
+	int last_digit;
 
-	return (last_digit);
-}
+	if (n > 0)
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = -1 * (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit); 
+	}
 
