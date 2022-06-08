528ed8c0804252bfba1a4d1f2767ac5b7a25ed6a
haymanottse11 <haymanottse11@gmail.com>
2022-06-08 02:53:53 -0700

    nested loop projects

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 = prints 10 times the alphabet, in lowercase
+ * allwed to use _putchar 2x maximum
+(*
+ *return 0 on success
+ */
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	char ch = 'a';
+
+	while( i <= 9)
+	{
+		for(ch = 'a' ; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+	i++
+}
