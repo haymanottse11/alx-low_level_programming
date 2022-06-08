c87fc332c13c3aff416f0f6e7c7abafca06513ab
haymanottse11 <haymanottse11@gmail.com>
2022-06-08 06:59:00 -0700

    nested loops projects

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign(int n) function prints + and returns 1 if n is positive
+ * ,prints 0 and return 0 if n is zero, and
+ * prints - and return -1 if n is negative.
+ */
+int print_sign(int n)
+{
+	if (n>0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
