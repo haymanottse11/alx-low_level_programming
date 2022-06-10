edf60de7d639f73b403ab41c6dabb5d4c933bb03
haymanottse11 <haymanottse11@gmail.com>
2022-06-09 19:16:45 -0700

    more on nested loops  projects

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
