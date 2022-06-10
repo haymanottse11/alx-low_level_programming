b67e2caa663b2b6b0a623c1dd6c764b8a115448a
haymanottse11 <haymanottse11@gmail.com>
2022-06-09 18:48:12 -0700

    more on nested loops projects

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isupper- check and print upperchase alphabets
- * @c - any character to be checked
+ * @c: any character to be checked
  *
  * Return: 1 if c is uppercase and 0 otherwise
  */
