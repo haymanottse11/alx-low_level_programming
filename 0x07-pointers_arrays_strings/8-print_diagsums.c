a417571a45724b7f74e75b0342ac700dfc6c556a
haymanottse11 <haymanottse11@gmail.com>
2022-06-21 19:39:18 -0700

    even more on printers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,8 +18,8 @@ void print_diagsums(int *a, int size)
 	a -= size;
 	for (index = 0; index < size; index++)
 	{
-		sum2 += a[index]
-			a -= size;
+		sum2 += a[index];
+		a -= size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
