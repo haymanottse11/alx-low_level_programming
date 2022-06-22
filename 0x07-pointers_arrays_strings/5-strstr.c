227a7097a443d4cd660d95cabab78df15e8eef78
haymanottse11 <haymanottse11@gmail.com>
2022-06-21 18:50:25 -0700

    a function that locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,22 +9,22 @@
  */
 char *_strstr(char *haystack, char *needle);
 {
-	int i;
-	int s = 0;
+	int index;
 
-	while (needle[s] != '\0')
-		s++;
+	if (*needle == 0)
+		return (haystack);
 	while (*haystack)
 	{
-		for (i = 0; needle[i]; i++)
+		index = 0;
+		if (haystack[index] == needle[index])
 		{
-			if (haystack[i] != needle[i])
-				break;
+			do{
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			}while (haystack[index] == needle[index]);
 		}
-		if (i != s)
-			haystack++;
-		else
-			return (haystack);
+		haystack++;
 	}
 	return ('\0');
 }
