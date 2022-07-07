b19b1ed4b09530c52c995405435638051a89293d
haymanottse11 <haymanottse11@gmail.com>
2022-07-06 19:21:02 -0700

    function executer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array == NULL || action == NULL)
 		return;
-	for (size = 0; size--;)
+	while (size-- > 0)
 	{
 		action(*array);
 		array++;
