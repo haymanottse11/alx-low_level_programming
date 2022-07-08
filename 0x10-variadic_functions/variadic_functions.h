8036da2d59a687ff71040c50a25744791ca28c56
haymanottse11 <haymanottse11@gmail.com>
2022-07-07 17:30:08 -0700

    summ them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,12 @@
 
 #include <stdarg.h>
 
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ * a data type corresponding to symbol.
+ */
 typedef struct printer
 {
 	char *symbol;
