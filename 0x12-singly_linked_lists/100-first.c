253103c92b14dc7c0a76bf3ccb44486ffaeffc9a
haymanottse11 <haymanottse11@gmail.com>
2022-07-15 15:34:35 -0700

    advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * hare - prints before main.
  */
-void __attribute__((constructor)) hare(void);
+void __attribute__((constructor)) hare()
 {
 	printf("You're beat! and yet, you must allow,\n"
 		"I bore my house upon my back!\n");
